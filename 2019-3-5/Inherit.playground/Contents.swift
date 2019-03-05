f2179428b007a5a3657dea07e8d2bf911a67c9a4
Leon Kang <kangleonjy@gmail.com>
2019-03-05 10:51:21 +0800

    new
    new

--- 2019-3-5/Inherit.playground/Contents.swift
+++ 2019-3-5/Inherit.playground/Contents.swift
@@ -1,3 +1,75 @@
 import UIKit
 
+class Vehicle{
+    var currentSpeed = 0.0
+    var description: String{
+        return "traveling at \(currentSpeed) miles per hour"
+    }
+    func makeNoise() {
+        
+    }
+}
+
+let someVehicle = Vehicle()
+
+print("Vehicle: \(someVehicle.description)")
+
+class Bicycle: Vehicle{
+    var hasBasket = false
+}
+
+let bicycle = Bicycle()
+bicycle.hasBasket = true
+
+bicycle.currentSpeed=15.0
+print("Bicycle: \(bicycle.description)")
+
+class Tandem: Bicycle{
+    var currentNumberOfPassengers = 0
+}
+
+let tandem = Tandem()
+tandem.hasBasket = true
+tandem.currentNumberOfPassengers = 2
+tandem.currentSpeed = 22.0
+print("Tandem: \(tandem.description)")
+
+class Train: Vehicle{
+    override func makeNoise() {
+        print("Choo Choo")
+    }
+}
+
+let train = Train()
+train.makeNoise()
+
+//你可以通过在你的子类重写里为继承而来的只读属性添加Getter和Setter来把它用作可读写属性。总之，你不能把一个继承而来的可读写属性表示为只读属性。
+
+//如果你提供了一个setter作为属性重写的一部分，你也就必须为重写提供一个getter。
+class Car: Vehicle{
+    var gear = 1
+    override var description: String{
+        return super.description + "in gear \(gear)"
+    }
+}
+
+let car = Car()
+car.currentSpeed = 25.0
+car.gear = 3
+print("Car: \(car.description)")
+
+//也要注意你不能为同一个属性同时提供重写的setter和重写的属性观察器。如果你想要监听属性值的改变，并且你已经为那个属性提供了一个自定义的setter，那么你从自定义的setter里就可以监听任意值的改变。
+class AutomaticCar: Car{
+    override var currentSpeed: Double{
+        didSet{
+            gear = Int(currentSpeed / 10.0) + 1
+        }
+    }
+}
+
+let automatic = AutomaticCar()
+automatic.currentSpeed=35.0
+print("AutomaticCar: \(automatic.description)")
+
+
 
