13bc9c177a6cf9639c036aa2706301e5a41cfd7d
Leon Kang <kangleonjy@gmail.com>
2019-04-09 10:11:43 +0800

    new
    new

---
+++ 2019-4-1/LiquidMenu/LiquidMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiquidMenu
+//
+//  Created by 吉腾蛟 on 2019/4/8.
+//  Copyright © 2019 JiYoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2019-4-1/LiquidMenu/LiquidMenuTests/LiquidMenuTests.swift
@@ -0,0 +1,34 @@
+//
+//  LiquidMenuTests.swift
+//  LiquidMenuTests
+//
+//  Created by 吉腾蛟 on 2019/4/8.
+//  Copyright © 2019 JiYoung. All rights reserved.
+//
+
+import XCTest
+@testable import LiquidMenu
+
+class LiquidMenuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
