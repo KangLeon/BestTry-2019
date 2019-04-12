be91699d038f2d5466ae1f034db533439d68ab8f
Leon Kang <kangleonjy@gmail.com>
2019-04-12 10:14:29 +0800

    new
    new

---
+++ 2019-4-12/LearnXib/LearnXib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnXib
+//
+//  Created by 吉腾蛟 on 2019/4/11.
+//  Copyright © 2019 JiYoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
