881898b92bfe5307957c7b96ddd4f03163ca4e17
jitengjiao <tengjiao.ji@dragonsoftbravo.com>
2019-06-02 23:36:24 +0800

    new
    new

---
+++ 2019-5-29/Restructure/Restructure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restructure
+//
+//  Created by admin on 2019/6/2.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2019-5-29/View Switcher/View Switcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  View Switcher
+//
+//  Created by admin on 2019/6/2.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+import UIKit
+
+class SwitchingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
