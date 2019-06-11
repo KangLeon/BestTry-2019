cb8331da7cc661eb0cb9d1685414ac94d94bfbff
jitengjiao <tengjiao.ji@dragonsoftbravo.com>
2019-06-11 21:41:25 +0800

    new
    new

---
+++ 2019-6-10/SwiftFightAgain/ArrayType.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var someInts = [Int]()
+print("someInts is of type [Int] with \(someInts.count) items.")
+
+someInts.append(3)
+
+someInts = []
+
+var thresDoubles = Array(repeating: 0.0, count: 3)
+
+var anotherThreeDoubles = Array(repeating: 2.5, count: 3)
+
+var sixDoubles = thresDoubles + anotherThreeDoubles
+
+var shoppingList: [String] = ["Eggs", "Milk"]
+
+var anotherShoppingList = ["Eggs","Milk"]
+
+print("The shopping list contains \(shoppingList.count) items.")
+
+
+
