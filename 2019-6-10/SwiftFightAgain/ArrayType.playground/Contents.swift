737f17d74e715bc5f928cb09a5b3f4c919ee2d60
Leon Kang <kangleonjy@gmail.com>
2019-06-12 18:41:56 +0800

    new
    new

--- 2019-6-10/SwiftFightAgain/ArrayType.playground/Contents.swift
+++ 2019-6-10/SwiftFightAgain/ArrayType.playground/Contents.swift
@@ -21,5 +21,31 @@ var anotherShoppingList = ["Eggs","Milk"]
 
 print("The shopping list contains \(shoppingList.count) items.")
 
+if shoppingList.isEmpty {
+    print("The shopping list is empty.")
+}else{
+    print("The shopping list is not empty.")
+}
+
+shoppingList.append("Flour")
+
+shoppingList += ["Baking Powder"]
+
+shoppingList += ["Chocolate Spread","Cheese","Butter"]
+
+var firstItem = shoppingList[0]
+
+shoppingList[0] = "Six eggs"
+
+shoppingList[4...6]=["Bananas","Apples"]
+
+shoppingList.insert("Maple Syrup", at: 0)
+
+let mapleSyrup = shoppingList.remove(at: 0)
+
+firstItem = shoppingList[0]
+
+let apples = shoppingList.removeLast()
+
 
 
