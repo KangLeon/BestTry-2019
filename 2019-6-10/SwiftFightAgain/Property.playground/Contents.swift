df333210c581a0f7b4cff06ed70e748e354dcc52
jitengjiao <tengjiao.ji@dragonsoftbravo.com>
2019-06-18 23:42:10 +0800

    new
    new

--- 2019-6-10/SwiftFightAgain/Property.playground/Contents.swift
+++ 2019-6-10/SwiftFightAgain/Property.playground/Contents.swift
@@ -2,3 +2,80 @@ import UIKit
 
 var str = "Hello, playground"
 
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue = 6
+
+class DataImporter {
+    var fileName = "data.txt"
+}
+
+class DataManager {
+    lazy var importer = DataImporter ()
+    var data = [String] ()
+}
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+
+print(manager.importer.fileName)
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        
+        set(newCenter) {
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+}
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0), size: Size(width: 10.0, height: 10.0))
+let initalSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+print("square.origin is now at (\(square.origin.x),\(square.origin.y))")
+
+struct CompactRect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get{
+          return Point(x: origin.x+(size.width/2), y: origin.y+(size.height/2))
+        }
+        set{
+            origin.x = newValue.x - (size.width/2)
+            origin.y = newValue.y - (size.height/2)
+        }
+    }
+}
+
+struct Cuboid {
+    var width = 0.0, height = 0.0, depth = 0.0
+    var volume: Double {
+     return width * height * depth
+    }
+}
+
+let fourByfiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+print("the volume of fourBuFiveByTwo is \(fourByfiveByTwo.volume)")
+
