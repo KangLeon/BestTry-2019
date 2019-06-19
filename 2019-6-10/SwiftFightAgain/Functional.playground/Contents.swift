7fb1f7e238f5de5b129653b3bfcb666a0fe910b3
jitengjiao <tengjiao.ji@dragonsoftbravo.com>
2019-06-19 23:43:02 +0800

    new
    new

--- 2019-6-10/SwiftFightAgain/Functional.playground/Contents.swift
+++ 2019-6-10/SwiftFightAgain/Functional.playground/Contents.swift
@@ -70,5 +70,51 @@ var ovemLight = TriStateSwitch.low
 ovemLight.next()
 ovemLight.next()
 
+struct LevelTracker {
+    static var highestUnLockedLevel = 1
+    var currentLevel = 1
+    
+    static func unlock(_ level: Int){
+        if level > highestUnLockedLevel {
+            highestUnLockedLevel = level
+        }
+    }
+    
+    static func isUnlocked(_ level: Int) -> Bool {
+        return level <= highestUnLockedLevel
+    }
+    
+    @discardableResult
+    mutating func advance(to level: Int) -> Bool {
+        if LevelTracker.isUnlocked(level) {
+            currentLevel = level
+            return true
+        }else{
+            return false
+        }
+    }
+}
 
+class Player {
+    var tracker = LevelTracker()
+    let playerName: String
+    func complete(level: Int) {
+        LevelTracker.unlock(level + 1)
+        tracker.advance(to: level + 1)
+    }
+    init(name: String) {
+        playerName = name
+    }
+}
+
+var player = Player(name: "Argyrios")
+player.complete(level: 1)
+print("highest unlocked level is now \(LevelTracker.highestUnLockedLevel)")
+
+player = Player(name: "Beto")
+if player.tracker.advance(to: 6){
+    print("player is now on level 6")
+}else{
+    print("level 6 has not yet been unlocked")
+}
 
