c0ffb9baac8d42f81e969ca92dbd3d0c26f0b512
jitengjiao <tengjiao.ji@dragonsoftbravo.com>
2019-06-19 22:02:24 +0800

    new
    new

--- 2019-6-10/SwiftFightAgain/Property.playground/Contents.swift
+++ 2019-6-10/SwiftFightAgain/Property.playground/Contents.swift
@@ -79,3 +79,88 @@ struct Cuboid {
 let fourByfiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
 print("the volume of fourBuFiveByTwo is \(fourByfiveByTwo.volume)")
 
+class StepCounter {
+    var totalSteps: Int = 0{
+        willSet(newTotalSteps){
+            print("About to set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+
+let stepCounter = StepCounter()
+stepCounter.totalSteps=200
+
+stepCounter.totalSteps=360
+
+stepCounter.totalSteps=896
+
+struct SomeStructure {
+    static var storedTypeProperty = "Some Value."
+    static var computedTypeProperty: Int {
+        return 1
+    }
+}
+
+enum SomeEnumeration{
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 6
+    }
+}
+
+class SomeClass {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        return 27
+    }
+    class var overrideableComputedTypeProperty: Int {
+        return 107
+    }
+}
+
+print(SomeStructure.storedTypeProperty)
+
+SomeStructure.storedTypeProperty = "Another value."
+
+print(SomeStructure.storedTypeProperty)
+
+print(SomeEnumeration.computedTypeProperty)
+
+print(SomeClass.computedTypeProperty)
+
+struct AudioChannel {
+    static let thresholdLevel = 10
+    static var maxInputLevelForAllChannels = 0
+    var currentLevel: Int = 0 {
+        didSet {
+            if currentLevel > AudioChannel.thresholdLevel {
+                currentLevel = AudioChannel.thresholdLevel
+            }
+            if currentLevel > AudioChannel.maxInputLevelForAllChannels {
+                AudioChannel.maxInputLevelForAllChannels = currentLevel
+            }
+        }
+    }
+}
+
+var leftChannel = AudioChannel()
+var rightChannel = AudioChannel()
+
+leftChannel.currentLevel = 7
+print(leftChannel.currentLevel)
+
+print(AudioChannel.maxInputLevelForAllChannels)
+
+rightChannel.currentLevel = 11
+
+print(rightChannel.currentLevel)
+
+print(AudioChannel.maxInputLevelForAllChannels)
+
+
+
