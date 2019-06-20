fede294cf358d9133e7fc6218bc4844c13e86d91
jitengjiao <tengjiao.ji@dragonsoftbravo.com>
2019-06-20 23:29:35 +0800

    new
    new

---
+++ 2019-6-10/SwiftFightAgain/Init.playground/Contents.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+var str = "Hello, playground"
+
+struct Fahrenheit {
+    var temperature: Double
+    init() {
+        temperature = 32.0
+    }
+}
+
+var f = Fahrenheit()
+
+print("The default temperature is \(f.temperature) Fahrenheit")
+
+struct FahrenheitSingle {
+    var temperature = 32.0
+}
+
+struct Celsius {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+
+struct Color {
+    let red, green, blue: Double
+    init(R red: Double, G green: Double, B blue: Double) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+    init(white: Double) {
+        red = white
+        green = white
+        blue = white
+    }
+}
+
+let magenta = Color(R: 1.0, G: 0.0, B: 1.0)
+let halfGray = Color(white: 0.5)
+
+struct CelsiusNone {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+    init(_ celsius: Double) {
+        temperatureInCelsius = celsius
+    }
+}
+
+let bodyTemperature = CelsiusNone(37.0)
+
+
+
