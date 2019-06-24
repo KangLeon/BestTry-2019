98aecd783f354a5d570d1658425486dcbbd7f05e
Leon Kang <kangleonjy@gmail.com>
2019-06-24 16:53:23 +0800

    new
    new

---
+++ 2019-6-24/ShapeProgress/ShapeProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShapeProgress
+//
+//  Created by 吉腾蛟 on 2019/6/24.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2019-6-24/ShapeProgress/ShapeProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShapeProgress
+//
+//  Created by 吉腾蛟 on 2019/6/24.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
