4ed6d44d1cd736981fe8c0660528e926ec477719
Leon Kang <kangleonjy@gmail.com>
2019-07-01 16:40:29 +0800

    new
    new

---
+++ 2019-6-10/SwiftFightAgain/AccessControl.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+
+public class SomePublicClass {
+    public var somePublicProperty = 0
+    var someInternalProperty = 0
+    fileprivate func someFilePrivateMethod() {}
+    private func somePrivateMethod() {}
+}
+
+
--- 2019-6-10/SwiftFightAgain/Generic.playground/Contents.swift
+++ 2019-6-10/SwiftFightAgain/Generic.playground/Contents.swift
@@ -55,7 +55,175 @@ stackOfStrings.push("cautro")
 
 let fromTheTop = stackOfStrings.pop()
 
+extension Stack {
+    var topItem: Element? {
+        return items.isEmpty ? nil : items[items.count - 1]
+    }
+}
 
+if let topItem = stackOfStrings.topItem {
+    print("The top item on the stack is \(topItem).")
+}
 
+func findIndex(ofString valueToFind: String, in array: [String]) -> Int? {
+    for (index, value) in array.enumerated() {
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
 
+let strings = ["cat", "dog", "llama", "parakeet", "terrapin"]
+if let foundIndex = findIndex(ofString: "llama", in: strings){
+    print("The index of llama is \(foundIndex)")
+}
 
+func findIndex<T: Equatable>(of valueToFind: T, in array:[T]) -> Int? {
+    for (index, value) in array.enumerated() {
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+
+let doubleIndex = findIndex(of: 9.3, in: [3.14159, 0.1, 0.25])
+
+let stringIndex = findIndex(of: "Andrea", in: ["Mike", "Malcolm", "Andrea"])
+
+protocol Container {
+    associatedtype ItemType
+    mutating func append(_ item: ItemType)
+    var count: Int {get}
+    subscript(i: Int) -> ItemType {get}
+}
+
+struct IntStackAssociated: Container {
+    var items = [Int]()
+    mutating func push (_ item: Int) {
+        items.append(item)
+    }
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+    typealias ItemType = Int
+    mutating func append(_ item: Int) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Int {
+        return items[i]
+    }
+}
+
+struct Satck<Element>: Container {
+    var items = [Element]()
+    mutating func push(_ item: Element){
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+    mutating func append(_ item: Satck<Element>.ItemType) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i :Int) -> Element {
+        return items[i]
+    }
+}
+
+//protocol SuffixableContainer: Container {
+//    associatedtype Suffix: SuffixableContainer where Suffix.Item == Item
+//    func suffix(_ size: Int) -> Suffix
+//}
+//
+//extension Stack: SuffixableContainer {
+//    func suffix(_ size: Int) -> Stack<Element>.Suffix {
+//        var result = Stack()
+//        for index in (count-size)..<count {
+//            result.append(self[index])
+//        }
+//        return result
+//    }
+//}
+//
+//var stackOfInts = Stack<Int>()
+//stackOfInts.append(10)
+//stackOfInts.append(20)
+//stackOfInts.append(30)
+//let suffix = stackOfInts.suffix(2)
+
+func allItemsMatch<C1: Container, C2: Container>(_ someContainer: C1, _ anotherContainer: C2) -> Bool where C1.ItemType == C2.ItemType, C1.ItemType: Equatable {
+    if someContainer.count != anotherContainer.count {
+        return false
+    }
+    
+    for i in 0..<someContainer.count {
+        if someContainer[i] != anotherContainer[i] {
+            return false
+        }
+    }
+    
+    return true
+}
+
+var stackOfStringsWhere = Stack<String>()
+stackOfStringsWhere.push("uno")
+stackOfStringsWhere.push("dos")
+stackOfStringsWhere.push("tres")
+
+var arrayOfStrings = ["uno", "dos", "tres"]
+
+//if allItemsMatch(stackOfStringsWhere, arrayOfStrings) {
+//    print("All items match.")
+//}else{
+//    print("Not all items match.")
+//}
+
+extension Stack where Element: Equatable {
+    func isTop(_ item: Element) -> Bool {
+        guard let topItem = items.last else {
+            return false
+        }
+        return topItem == item
+    }
+}
+
+if stackOfStringsWhere.isTop("tres") {
+    print("Top is tres")
+}else{
+    print("Top element is something else.")
+}
+
+//extension Container where Item: Equatable {
+//    func startsWith(_ item: Item) -> Bool {
+//        return count >= 1 && self[0] == item
+//    }
+//}
+
+protocol ContainerWhere {
+    associatedtype Item
+    mutating func append(_ item: Item)
+    var count: Int {get}
+    subscript(i: Int) -> Item { get }
+    
+    associatedtype Iterator: IteratorProtocol where Iterator.Element == Item
+    func makeIterator() -> Iterator
+}
+
+extension ContainerWhere {
+    subscript<Indices: Sequence>(indices: Indices) -> [Item]
+        where Indices.Iterator.Element == Int {
+            var result = [Item]()
+            for index in indices {
+                result.append(self[index])
+            }
+            return result
+    }
+}
