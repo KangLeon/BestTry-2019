3048ad60620095c390ca874fd8035deea773b9ac
Leon Kang <kangleonjy@gmail.com>
2019-07-03 14:10:44 +0800

    new
    new

--- 2019-7-2/SwiftFun/SwiftFun/ViewController.swift
+++ 2019-7-2/SwiftFun/SwiftFun/ViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     private let label = UILabel()
-    
-    private 
+    private let testView = UIView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+    private let testView2 = UIView(frame: CGRect(x: 100, y: 300, width: 200, height: 200))
+    private let button = UIButton(type: .detailDisclosure)
+    private let imageView = UIImageView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+    private let textField = UITextField(frame: CGRect(x: 50, y: 100, width: 300, height: 30))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +33,56 @@ class ViewController: UIViewController {
         label.isUserInteractionEnabled=true
         
 //        view.addSubview(label)
+        
+        testView.backgroundColor=UIColor.white
+//        view.addSubview(testView)
+//        print("view frmae is \(testView.frame)")
+//        print("view.bounds is \(testView.bounds)")
+        
+        testView2.backgroundColor=UIColor.gray
+//        view.addSubview(testView2)
+        
+        let tap1 = UITapGestureRecognizer(target: self, action: #selector(handleTap1))
+        let tap2 = UITapGestureRecognizer(target: self, action: #selector(handleTap2))
+        
+        testView.tag=1
+        testView2.tag=2
+        
+        testView.addGestureRecognizer(tap1)
+        testView2.addGestureRecognizer(tap2)
+        
+        for subView in view.subviews {
+            if subView.tag==1{
+                print("subview`s frame \(subView.frame)")
+            }
+        }
+        
+        button.frame=CGRect(x: 50, y: 100, width: 300, height: 100)
+        button.backgroundColor=UIColor.red
+        button.setTitle("Hello", for: .normal)
+        
+        button.addTarget(self, action: #selector(clickAction), for: .touchUpInside)
+        
+//        view.addSubview(button)
+        
+        imageView.backgroundColor=UIColor.red
+        imageView.contentMode = .scaleAspectFit
+        
+//        view.addSubview(imageView);
+        
+        textField.backgroundColor=UIColor.clear
+        textField.borderStyle = .roundedRect
+        if textField.isEditing {
+            print("isEditing is true")
+        }else{
+            print("isEditing is false")
+        }
+        textField.placeholder="请输入您的手机号"
+        textField.clearButtonMode = .always
+        textField.isSecureTextEntry = true
+        textField.delegate = self
+        
+        view.addSubview(textField)
     }
 
     @objc func changeTextContent() {
@@ -40,6 +93,34 @@ class ViewController: UIViewController {
         }
     }
     
+    @objc func handleTap1() {
+        print("testview tag is \(testView.tag)")
+    }
     
+    @objc func handleTap2 () {
+        print("testview2 tag is \(testView2.tag)")
+    }
+    
+    @objc func clickAction () {
+        print("点击了按钮!")
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    //设置是否可以开始编辑
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    //返回键是否可按
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //结束编辑
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("text field end editing")
+    }
 }
 
