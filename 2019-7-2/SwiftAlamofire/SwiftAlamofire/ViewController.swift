690fa2e7797c7be07415323f5fbe21211b234cef
Leon Kang <kangleonjy@gmail.com>
2019-07-03 18:03:16 +0800

    new
    new

---
+++ 2019-7-2/SwiftAlamofire/SwiftAlamofire/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftAlamofire
+//
+//  Created by 吉腾蛟 on 2019/7/3.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        UploadRequest()
+        
+    }
+
+
+}
+
