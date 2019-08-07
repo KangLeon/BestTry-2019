2d969e4ea7031e2e3285ce2d6b7897da5bcabbc2
Leon Kang <kangleonjy@gmail.com>
2019-08-07 17:09:54 +0800

    new
    new

---
+++ 2019-8-6/AVPlayerViewController/AVPlayerViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVPlayerViewController
+//
+//  Created by 吉腾蛟 on 2019/8/7.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 2019-8-6/AVPlayerViewController/AVPlayerViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AVPlayerViewController
+//
+//  Created by 吉腾蛟 on 2019/8/7.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
