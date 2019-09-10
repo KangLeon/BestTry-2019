63f5db6d7128e3980939fef1fbcb94825d602077
Leon Kang <kangleonjy@gmail.com>
2019-09-10 18:09:10 +0800

    new
    new

--- 2019-5-29/View Switcher/View Switcher/BlueViewController.swift
+++ 2019-5-29/View Switcher/View Switcher/BlueViewController.swift
@@ -16,6 +16,13 @@ class BlueViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func blueButtonPressed(sender: UIButton){
+        let alert = UIAlertController(title: "Blue View button Pressed", message: "You pressed the button on the blue view", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yep, I did", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
--- 2019-5-29/View Switcher/View Switcher/YellowViewController.swift
+++ 2019-5-29/View Switcher/View Switcher/YellowViewController.swift
@@ -16,7 +16,13 @@ class YellowViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func yellowButtonPressed(sender: UIButton){
+        let alert = UIAlertController(title: "Yellow View Button Pressed", message: "You pressed the button on the yellow view", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yep, I did", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
