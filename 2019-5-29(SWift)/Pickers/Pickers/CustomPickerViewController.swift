a12f97a34a18e812b731ed53eb35a4559af71d99
Leon Kang <kangleonjy@gmail.com>
2019-09-12 10:00:12 +0800

    我又变强了！！！
    我又变强了！！！

--- 2019-5-29(SWift)/Pickers/Pickers/CustomPickerViewController.swift
+++ 2019-5-29(SWift)/Pickers/Pickers/CustomPickerViewController.swift
@@ -8,15 +8,68 @@
 
 import UIKit
 
-class CustomPickerViewController: UIViewController {
-
+class CustomPickerViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    
+    private var images : [UIImage]!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var winLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        images = [UIImage(named: "seven")!,
+                  UIImage(named: "bar")!,
+                  UIImage(named: "crown")!,
+                  UIImage(named: "cherry")!,
+                  UIImage(named: "lemon")!,
+                  UIImage(named: "apple")!
+        ]
+        winLabel.text = " "
     }
     
-
+    @IBAction func spin(_ sender: Any) {
+        var win = false
+        var numInRow = -1
+        var lastVal = -1
+        
+        for i in 0..<5 {
+            let newValue = Int(arc4random_uniform(UInt32(images.count)))
+            if newValue == lastVal {
+                numInRow+=1
+            }else{
+                numInRow = 1
+            }
+            
+            lastVal = newValue
+            
+            picker.selectRow(newValue, inComponent: i, animated: true)
+            picker.reloadComponent(i)
+            if numInRow >= 3 {
+                win = true
+            }
+        }
+        
+        winLabel.text = win ? "WINNER!" : " "
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 5
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return images.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let image = images[row]
+        let imageView = UIImageView(image: image)
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 64
+    }
     /*
     // MARK: - Navigation
 
