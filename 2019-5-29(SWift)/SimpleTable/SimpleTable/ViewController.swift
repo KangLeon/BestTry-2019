31e14819efe29969de292ddba5c6f22ff89dc2dd
Leon Kang <kangleonjy@gmail.com>
2019-09-16 11:24:57 +0800

    我又变强了！！！
    我又变强了！！！

--- 2019-5-29(SWift)/SimpleTable/SimpleTable/ViewController.swift
+++ 2019-5-29(SWift)/SimpleTable/SimpleTable/ViewController.swift
@@ -14,19 +14,67 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     let simpleTableIdentifier = "SimpleTableIdentidier"
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return dwarves.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: simpleTableIdentifier)
+        }
+        
+        let image = UIImage(named: "star")
+        cell?.imageView?.image=image
+        let highlightedImage = UIImage(named: "star2")
+        cell?.imageView?.highlightedImage=highlightedImage
+        
+        if indexPath.row < 7 {
+            cell?.detailTextLabel?.text="Mr disney"
+        }else{
+            cell?.detailTextLabel?.text="Mr Tolkien"
+        }
+        
+        cell?.textLabel?.text = dwarves[indexPath.row]
+        cell?.textLabel?.font = UIFont.boldSystemFont(ofSize: 50)
+        
+        return cell!
     }
     
+    func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
+        return indexPath.row % 4
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if indexPath.row == 0 {
+            return nil
+        }else if (indexPath.row%2 == 0){
+            return IndexPath(row: indexPath.row+1, section: indexPath.section)
+        }else{
+            return indexPath
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let rowValue = dwarves[indexPath.row]
+        let message = "You selected \(rowValue)"
+        
+        let controller = UIAlertController(title: "Row Selected", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Yes", style: .default, handler: nil)
+        
+        controller.addAction(action)
+        
+        present(controller, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.row == 0 ? 120 : 70
+    }
 }
 
