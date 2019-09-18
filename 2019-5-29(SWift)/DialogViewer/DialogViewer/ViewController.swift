15342b9aa34d309537b31778260b7eae8d469e5f
Leon Kang <kangleonjy@gmail.com>
2019-09-18 11:10:23 +0800

    我又变强了！！！
    我又变强了！！！

---
+++ 2019-5-29(SWift)/DialogViewer/DialogViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DialogViewer
+//
+//  Created by 吉腾蛟 on 2019/9/18.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
