a185f865b5190cf0b27231c329e280e36c9072b9
Leon Kang <kangleonjy@gmail.com>
2019-09-19 18:46:09 +0800

    我又变强了！！！
    我又变强了！！！

--- 2019-5-29(SWift)/Persistence/Persistence/ViewController.swift
+++ 2019-5-29(SWift)/Persistence/Persistence/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lineFields: [UITextField]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let filePath = self.dataFilePath()
+        if FileManager.default.fileExists(atPath: filePath) {
+            let array = NSArray(contentsOfFile: filePath) as! [String]
+            for index in 0..<array.count {
+                lineFields[index].text = array[index]
+            }
+        }
+        let app = UIApplication.shared
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationWillResignActive(_ :)), name: NSNotification.Name(rawValue: "UIApplicationWillResignActiveNotification"), object: app)
+        
     }
 
-
+    func dataFilePath() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentDirectory = paths[0] as NSString
+        return documentDirectory.appendingPathComponent("data.plist") as String
+        
+    }
 }
 
