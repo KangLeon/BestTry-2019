60751db66c64fa6ded0a4cd66b2e33249da36033
Leon Kang <kangleonjy@gmail.com>
2019-09-19 14:28:51 +0800

    我又变强了！！！
    我又变强了！！！

---
+++ 2019-5-29(SWift)/BridgeControl/BridgeControl/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  BridgeControl
+//
+//  Created by 吉腾蛟 on 2019/9/19.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+import Foundation
+
+let officerKey = "officer"
+let authorizationCodeKey = "authorizationCode"
+let rankKey = "rank"
+let warpDriveKey = "warp"
+let warpFactorKey = "warpFactor"
+let favoriteTeaKey = "favoriteTea"
+let favoriteCaptainKey = "favoriteCaptain"
+let favoriteGadgetKey = "favoriteGadget"
+let favoriteAlienKey = "favoriteAlien"
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2019-5-29(SWift)/BridgeControl/BridgeControl/FirstViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  BridgeControl
+//
+//  Created by 吉腾蛟 on 2019/9/19.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet var officerLabel: UILabel!
+    @IBOutlet var authorizationCodeLabel: UILabel!
+    @IBOutlet var rankLabel: UILabel!
+    @IBOutlet var warpDriveLabel: UILabel!
+    @IBOutlet var warpFactorLabel: UILabel!
+    @IBOutlet var favoriteTeaLabel: UILabel!
+    @IBOutlet var favoriteCaptainLabel: UILabel!
+    @IBOutlet var favoriteGadgetLabel: UILabel!
+    @IBOutlet var favoriteAlienLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func refreshFields() {
+        let defaults = UserDefaults.standard
+        officerLabel.text = defaults.string(forKey: officerKey)
+        authorizationCodeLabel.text = defaults.string(forKey: authorizationCodeKey)
+        rankLabel.text = defaults.string(forKey: authorizationCodeKey)
+        warpDriveLabel.text = defaults.bool(forKey: warpDriveKey) ? "Engaged" : "Disabled"
+        warpFactorLabel.text = (defaults.object(forKey: warpFactorLabel) as! NSNumber)
+        favoriteTeaLabel.text = defaults.string(forKey: favoriteTeaKey)
+        favoriteCaptainLabel.text = defaults.string(forKey: favoriteCaptainKey)
+        favoriteGadgetLabel.text = defaults.string(forKey: favoriteGadgetKey)
+        favoriteAlienLabel.text = defaults.string(forKey: favoriteAlienKey)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshFields()
+    }
+}
+
