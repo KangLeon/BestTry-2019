5e975f384cf4f436313dd2e3d96d4a39f22b60b7
Leon Kang <kangleonjy@gmail.com>
2019-09-25 14:58:01 +0800

    我又变强了！！！
    我又变强了！！！

--- 2019-5-29(SWift)/TouchExplorer/TouchExplorer/ViewController.swift
+++ 2019-5-29(SWift)/TouchExplorer/TouchExplorer/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var tapsLabel: UILabel!
+    @IBOutlet weak var touchesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    func updateLabelsFromTouches(touches: Set<UITouch>!) {
+        let touch = touches.randomElement()!
+        let numTaps = touch.tapCount
+        let tapMessage = "\(numTaps) taps detected"
+        tapsLabel.text = tapMessage
+        
+        let numTouches = touches.count
+        let touchMsg = "\(numTouches) touches detected"
+        touchesLabel.text = touchMsg
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Began"
+        updateLabelsFromTouches(touches: event?.allTouches)
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Cancelled"
+        updateLabelsFromTouches(touches: event?.allTouches)
     }
 
-
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Ended"
+        updateLabelsFromTouches(touches: event?.allTouches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Drag Detected"
+        updateLabelsFromTouches(touches: event?.allTouches)
+    }
 }
 
