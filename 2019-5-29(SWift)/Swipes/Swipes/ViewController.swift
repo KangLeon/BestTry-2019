ad723b935b83deaac590493c879194b14cc33a9b
Leon Kang <kangleonjy@gmail.com>
2019-09-26 15:43:01 +0800

    我又变强了！！！
    我又变强了！！！

--- 2019-5-29(SWift)/Swipes/Swipes/ViewController.swift
+++ 2019-5-29(SWift)/Swipes/Swipes/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    private var gestureStartPoint: CGPoint!
+    
+    private let minimumGestureLength = Float(25.0)
+    private let maximumVariance = Float(5)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.randomElement()!
+        gestureStartPoint = touch.location(in: self.view)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.randomElement()!
+        let currentPosition = touch.location(in: self.view)
+        
+        
+    }
 }
 
