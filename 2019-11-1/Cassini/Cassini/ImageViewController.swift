39e2f44c741794631fc392b7f28c9378aa3afc42
Leon Kang <kangleonjy@gmail.com>
2019-11-01 17:53:20 +0800

    new
    new

--- 2019-11-1/Cassini/Cassini/ImageViewController.swift
+++ 2019-11-1/Cassini/Cassini/ImageViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ImageViewController: UIViewController {
 
+    var imageURL: URL? {
+        didSet {
+            imageView.image=nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if imageURL == nil {
+            imageURL = DemoURLs.stanford
+        }
     }
     
-
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                imageView.image = UIImage(data: imageData)
+            }
+        }
+    }
 }
