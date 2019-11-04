518743b6c70e1a798b1e1b790058e5ed673ac513
Leon Kang <kangleonjy@gmail.com>
2019-11-04 17:54:19 +0800

    new
    new

---
+++ 2019-11-1/EmojiArt/EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by 吉腾蛟 on 2019/11/4.
+//  Copyright © 2019 JY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
