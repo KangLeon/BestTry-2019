abfbf0014179d7f5331f75215b23f5722972d955
Leon Kang <kangleonjy@gmail.com>
2019-11-04 11:06:59 +0800

    new
    new

--- 2019-11-1/Cassini/Cassini/ImageViewController.swift
+++ 2019-11-1/Cassini/Cassini/ImageViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
-class ImageViewController: UIViewController {
+class ImageViewController: UIViewController, UIScrollViewDelegate {
 
     var imageURL: URL? {
         didSet {
-            imageView.image=nil
+            image=nil
             if view.window != nil {
                 fetchImage()
             }
+        }
+    }
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView.contentSize = imageView.frame.size
         }
     }
     
@@ -26,7 +37,20 @@ class ImageViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!{
+        didSet {
+            scrollView.minimumZoomScale = 1/25
+            scrollView.maximumZoomScale = 1.0
+            scrollView.delegate = self
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    var imageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,7 +65,7 @@ class ImageViewController: UIViewController {
         if let url = imageURL {
             let urlContents = try? Data(contentsOf: url)
             if let imageData = urlContents {
-                imageView.image = UIImage(data: imageData)
+                image = UIImage(data: imageData)
             }
         }
     }
